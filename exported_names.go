cdc8776055b06b9f0a9c51bf7dae2d1df82d8bf9
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-15 20:40:42 +0300

    understanding exported names

---
+++ exported_names.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	/*
+		Pi büyük harfle başlıyor.
+		Exported (Public diyebiliriz) anlamındadır. Dışarıdan erişilebilir.
+	*/
+	fmt.Println(math.Pi)
+}
