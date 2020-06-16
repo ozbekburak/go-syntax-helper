ae79cc3d5dfb543452e5fa9b81ac06a7d9d653db
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-16 15:37:44 +0300

    fixed typo and add demonstration of shortened parameter type

--- 2_functions.go
+++ 2_functions.go
@@ -4,10 +4,15 @@ import "fmt"
 
 // direkt int kullanabilirken, float şeklinde veri tipi kullanamıyoruz.
 // seçeneklerimiz: float32, float64
-func add(x float32, y float32) float32 {
+func divide(x float32, y float32) float32 {
 	return x / y
 }
 
+func subtract(x, y int) int {
+	return x - y
+}
+
 func main() {
-	fmt.Println(add(42.5, 13.1))
+	fmt.Println(divide(42.5, 13.1))
+	fmt.Println(subtract(42, 13))
 }
