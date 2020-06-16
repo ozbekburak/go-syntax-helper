10c8d6cd906b973089ac26221f062854414be5c2
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-16 16:02:12 +0300

    shows variable declarations

---
+++ 4_variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// değişkenlerin paket veya fonksiyon seviyesinde olabildiğini görüyoruz.
+// fonksiyonların dışında tanımlanan bütün ifadeler (statement) keyword ile başlamalıdır. (var, func etc) ":=" kullanılamaz.
+
+var linux, windows bool      // without initializer
+var year, age int = 1995, 25 // with initializer
+var name = "Burak"           // initializer var ise, değişken tipi otomatik kendisi alır.
+
+func main() {
+	var apple int
+	city := "İstanbul" // short assignment.
+
+	fmt.Println(apple, linux, windows)
+	fmt.Println(name, year, age)
+	fmt.Println(city)
+}
