9d01e29aa88a89d4418037091c2dacbea347774f
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-17 16:16:26 +0300

    create constants

---
+++ 6_constants.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// sabit değişkenleri ":=" şeklinde tanımlayamayız!
+const PI = 3.14
+
+func main() {
+	fmt.Println("The one of the most important thing in math is the number of PI :", PI)
+}
