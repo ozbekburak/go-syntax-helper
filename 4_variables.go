18603debddb66413ca8d202bfc5fb0131d05ce67
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-17 15:54:40 +0300

    show variables declaration in a different way

--- 4_variables.go
+++ 4_variables.go
@@ -9,6 +9,12 @@ var linux, windows bool      // without initializer
 var year, age int = 1995, 25 // with initializer
 var name = "Burak"           // initializer var ise, değişken tipi otomatik kendisi alır.
 
+// değişken tanımları bloklar halinde yapılabilir.
+var (
+	isRaining bool   = false
+	movieName string = "Who Am I"
+)
+
 func main() {
 	var apple int
 	city := "İstanbul" // short assignment.
