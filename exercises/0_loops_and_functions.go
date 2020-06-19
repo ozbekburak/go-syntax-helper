24f281955822974b80c1afe0d210c7c9601f2707
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-19 15:57:22 +0300

    first version of exercise created

---
+++ exercises/0_loops_and_functions.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	for i := 1; i <= 10; i++ {
+		fmt.Println("Our z:", z)
+		z -= (z*z - x) / (2 * x)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+}
