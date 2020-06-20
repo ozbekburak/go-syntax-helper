0deec76ec7015ffbe1f1754f4672750ad6414964
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-20 15:27:42 +0300

    an example about pointers done

---
+++ 14_pointers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// pointer, değerimizin hafızadaki (memory) adresini tutar.
+// pointerlarla ilk kez karşılaşanlar, akıllarında şu şekilde tutabilirler.
+// A kişisi size bir evi, arabayı, adresi sorabilir. Parmağınızla evi gösterebilirsiniz
+// parmağınız bu durumda ev, araba ya da sorulan adres değil, onlardan birini işaret eden araçtır. Yani "pointer"dır.
+// aynı şekilde pointerın pointerını da oluşturabilirsiniz. yani diğer parmağınızla işaret ettiğiniz parmağı işaret edebilirsiniz.
+// (tam adresini hatırlayamadığım ama stackoverflow'da okuduğunu düşündüğüm bir metafor)
+func main() {
+	myCar := "Black Mercedes"
+	// & ile adresi işaret ederken, * ile işaretçimizin (pointer) içeriğini okuruz
+	pointToMyCar := &myCar
+	fmt.Println("Address of my lovely car:", pointToMyCar)
+	fmt.Println("Car itself:", *pointToMyCar)
+	*pointToMyCar = "Red Porsche"
+	fmt.Println("My new car:", myCar)
+}
