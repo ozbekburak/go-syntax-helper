298a477960865c562d32171091dfaf3958a48175
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-20 10:31:22 +0300

    explain defer statement

---
+++ 12_defer.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// defer: parent (deferı kapsayan) fonksiyon bitinceye kadar çalıştırmak istemediğimiz fonksiyonlar için kullanırız.
+// örneğin, aşağıdaki örnekte hello ekrana yazdırıldıktan sonra world yazılacaktır.
+func main() {
+	defer fmt.Println("world")
+	fmt.Println("hello")
+}
