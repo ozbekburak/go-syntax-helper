ae1e2fa01f54849b0804247423de4e21ea2e6998
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-21 10:11:44 +0300

    explain slice reference to arrays

--- 17_slices.go
+++ 17_slices.go
@@ -6,10 +6,35 @@ import "fmt"
 // slicelar ise dinamik olarak boyutlandırılabilirler ve arraylerin elemanlarına esnek bir erişim imkanı verir
 func main() {
 	evenNumbers := [11]int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
-	// sliceların genel erişim sytaxi "array[low:high]" şeklindedir
-	// low aralığa dahil edilirken high edilmez. yani; aşağıdaki örnekte
-	// 0. eleman ekrana bastırılırken en son bastırılan sayımız 4.eleman olacaktır
+	/*
+		sliceların genel erişim sytaxi "array[low:high]" şeklindedir
+		low aralığa dahil edilirken high edilmez. yani; aşağıdaki örnekte
+		0. eleman ekrana bastırılırken en son bastırılan sayımız 4.eleman olacaktır
+	*/
 	var evenSingleDigitNumbers = evenNumbers[0:5] // evenNumbers [:5] şeklinde de gösterebiliriz
 	fmt.Println(evenSingleDigitNumbers)
+
+	books := [4]string{
+		"The Education of Will",
+		"Ward Six",
+		"Atomic Habits",
+		"The Power of Habit",
+	}
+	fmt.Println("The book list I've read:", books)
+
+	mayRead := books[0:3]
+	juneRead := books[2:] // 2.elemeandan itibaren listenin geri kalanını son elemanını da dahil ederek alır
+	fmt.Println("Books read in May:", mayRead)
+	fmt.Println("Books read in June", juneRead)
+
+	/*
+		aşağıdaki örnekte görebileceğiniz gibi, slicelar bir nevi arraylere referans verirler.
+		ilgili arrayin elemanını değiştiren slice, ona bağlı diğer slice değerlerini ve array değerini de
+		günceller!
+	*/
+	mayRead[2] = "The Pragmatic Programmer"
+	fmt.Println("Books read in May:", mayRead)
+	fmt.Println("Books read in June", juneRead)
+	fmt.Println("The book list I've read [updated]:", books)
 
 }
