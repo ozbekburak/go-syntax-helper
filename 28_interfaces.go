583a4fd53d493eea773d4455e9725d0365db98c7
Burak Kaan <ozbekburakkaan@outlook.com>
2020-06-26 16:11:47 +0300

    make an example about interfaces

--- 28_interfaces.go
+++ 28_interfaces.go
@@ -1,1 +1,33 @@
 package main
+
+import "fmt"
+
+// Writer arayüzü...
+// interface kavramı, kullanıldığı tipler tarafından kullanılmasını zorunlu kıldığı methodları içerir diyebiliriz
+type Writer interface {
+	WriteABook(s string)
+}
+
+func main() {
+	var isaacAsimov Writer
+	isaacAsimov = Writer(Novelist{"Isaac Asimov"})
+	robertFrost := Writer(Poet{"Robert Frost"})
+	isaacAsimov.WriteABook("I, Robot")
+	robertFrost.WriteABook("The Road Not Taken")
+}
+
+type Novelist struct {
+	name string
+}
+
+type Poet struct {
+	name string
+}
+
+func (novelist Novelist) WriteABook(s string) {
+	fmt.Println(novelist.name, "wrote a book which name is:", s)
+}
+
+func (poet Poet) WriteABook(s string) {
+	fmt.Println(poet.name, "wrote a poem which name is:", s)
+}
