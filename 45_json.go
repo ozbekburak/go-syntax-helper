d52cd3b4c49254f0c01dabd8e28d2fb6edff3323
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-06 20:42:34 +0300

    encode and decode json done

--- 45_json.go
+++ 45_json.go
@@ -20,21 +20,23 @@ func main() {
 	// News'e ait bir instance oluşturuyoruz
 	n := News{1, "Covid-19 Report", "Last reports show that Covid-19 still threats our health", "Reporter X", 129503943940}
 
+	// encode işlemi
 	resultOfEncodedData, err := encodeToJSON(n)
 	if err != nil {
 		fmt.Println("Someting went wrong: ", err)
 	}
-	fmt.Println("Your encoded json:\n", resultOfEncodedData)
+	fmt.Printf("Your encoded json: %v\n", resultOfEncodedData)
 
+	// decode işlemi
 	resultOfDecodedData, error := decodeJSON(resultOfEncodedData)
 	if error != nil {
-		fmt.Println("Someting went wrong: ", error)
+		fmt.Println("Something went wrong: ", error)
 	}
-	fmt.Println("Hey you come back to start: ", resultOfDecodedData)
+	fmt.Printf("-----------\nHey you successfully decode your json!: %v\n", *resultOfDecodedData)
 }
 
-// Aşağıdaki fonksiyon ile news'imizin json-encoded halini oluşturuyoruz
-// Json, genelde api lar ile çalışırken websitelerinin bize sunduğu veri formatı oalrak karşımıza çıkar
+// Aşağıdaki fonksiyon ile verimizin json-encoded halini oluşturuyoruz
+// Json, genelde api lar ile çalışırken websitelerinin bize sunduğu veri formatı olarak karşımıza çıkar
 func encodeToJSON(n News) ([]byte, error) {
 	encodedNews, err := json.Marshal(n)
 	if err != nil {
@@ -46,13 +48,16 @@ func encodeToJSON(n News) ([]byte, error) {
 }
 
 // does not work for now
-func decodeJSON(data []byte) (News, error) {
+func decodeJSON(data []byte) (*News, error) {
 	// JSON datamızı şimdi geri decode edelim
 	// İlk önce decode edilecek verinin saklanacağı bir alan oluşturmamız lazım
 	var decodedNews News
+
+	// Eğer verimiz (data []byte) decodedNews e uygun bir JSON verisiyle, error
+	// nil'e eşit olacaktır ve data'dan gelen veri decodedNews struct'ında saklanacaktır.
 	err := json.Unmarshal(data, &decodedNews)
 	if err != nil {
 		return nil, err
 	}
-	return decodedNews, nil
+	return &decodedNews, nil
 }
