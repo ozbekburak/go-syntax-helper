7dd8b64f6c4b7ff0ca95be2d3e5e75f897d4e4cb
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-09 15:28:49 +0300

    tags on struct

--- 47_json_bonus.go
+++ 47_json_bonus.go
@@ -10,6 +10,11 @@ type response1 struct {
 	Fruits []string
 }
 
+type response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
+
 func main() {
 	// slicelar için json encoding örneği
 	mySlice := []string{"monday", "tuesday", "wednesday"}
@@ -24,10 +29,20 @@ func main() {
 	// encoding/json paketi özel veri tiplerinizi otomatik olarak encode eder (örneğin aşağıdaki response1 custom bir type bizim için)
 	// encode edilmiş çıktılara yalnızca export edilmiş alanları dahil eder (yani büyük harfle tanımlanmış)
 	// ve varsayılan olarak bu adları JSON anahtarları olarak kullanır
+
 	// gobyexample.com/json
 	res1D := &response1{
 		Page:   1,
 		Fruits: []string{"apple", "peach", "pear"}}
 	res1B, _ := json.Marshal(res1D)
 	fmt.Println(string(res1B))
+
+	// struct alanlarında encode edilmiş verinin json keylerini etiketler (tag) ile
+	// customize edebiliyoruz. (bkz: __response2 struct__)
+	res2D := &response2{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res2B, _ := json.Marshal(res2D)
+	fmt.Println(string(res2B))
+
 }
