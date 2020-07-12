96eb5ec74678155c0e1cc35a654b12a902b01e6a
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-12 15:30:03 +0300

    init subcommands implementation

---
+++ 50_flag_subcommands.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+)
+
+// reference: https://www.digitalocean.com/community/tutorials/how-to-use-the-flag-package-in-go
+
+// birçok cli uygulaması, bir takım araçları tek komut altında toplamak için
+// genellikle alt komutlardan (sub-commands) faydalanır
+// her bir alt komutun kendi flag koleksiyonuna sahip olabildiğini bilmekte fayda var!
+
+func NewGreetCommand() *GreetCommand {
+	gc := &GreetCommand{
+		fs: flag.NewFlagSet("greet", flag.ContinueOnError),
+	}
+
+	gc.fs.StringVar(&gc.name, "name", "World", "name of the person to be greeted")
+
+	return gc
+}
+
+type GreetCommand struct {
+	fs *flag.FlagSet
+
+	name string
+}
+
+func (g *GreetCommand) Name() string {
+	return g.fs.Name()
+}
+
+func (g *GreetCommand) Init(args []string) error {
+	return g.fs.Parse(args)
+}
+
+func (g *GreetCommand) Run() error {
+	fmt.Println("Hello", g.name, "!")
+	return nil
+}
+
+type Runner interface {
+	Init([]string) error
+	Run() error
+	Name() string
+}
+
+func root(args []string) error {
+	if len(args) < 1 {
+		return errors.New("You must pass a sub-command")
+	}
+
+	cmds := []Runner{
+		NewGreetCommand(),
+	}
+	subcommand := os.Args[1]
+
+	for _, cmd := range cmds {
+		if cmd.Name() == subcommand {
+			cmd.Init(os.Args[2:])
+			return cmd.Run()
+		}
+	}
+
+	return fmt.Errorf("Unknown subcommand: %s", subcommand)
+}
+
+func main() {
+
+	if err := root(os.Args[1:]); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
