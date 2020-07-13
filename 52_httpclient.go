48016b6540954f6dbc996af239e2a069080083ea
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-13 16:08:00 +0300

    trying different response properties

--- 52_httpclient.go
+++ 52_httpclient.go
@@ -7,19 +7,21 @@ import (
 )
 
 func main() {
-	response, err := http.Get("http://www.reddit.com")
+	response, err := http.Get("https://dev.to")
 	if err != nil {
 		panic(err)
 	}
 	defer response.Body.Close()
 
 	fmt.Println(response.Status)
-	fmt.Printf("Response : %v", *response)
+	fmt.Println(*response.Request)
+	//fmt.Printf("Response : %v", *response)
 
 	scanner := bufio.NewScanner(response.Body)
-	fmt.Println("\n\nScanner: ", scanner.Text())
 	fmt.Println(scanner.Text())
-	for i := 0; i < 5; i++ {
-		fmt.Println(scanner.Text())
-	}
+	// fmt.Println("\n\nScanner: ", scanner.Text())
+	// fmt.Println(scanner.Text())
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(scanner.Text())
+	// }
 }
