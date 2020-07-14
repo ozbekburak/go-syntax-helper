f932bdefd32ac66e4a840cba3f232627bdefc76b
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-14 15:49:01 +0300

    create handler function

--- 53_httpserver.go
+++ 53_httpserver.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+// handlerlar http.handler interfaceini kullanan nesnelerdir
+// aşağıdaki hello fonksiyonumuz handler olarak servis veriyor
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
 func main() {
 
 }
