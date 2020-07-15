eb1445255b133ce8f32d604feb1ac9c953b4ba5e
Burak Kaan <ozbekburakkaan@outlook.com>
2020-07-15 22:00:23 +0300

    implement post function

--- 54_httppost.go
+++ 54_httppost.go
@@ -1,10 +1,23 @@
 package main
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func main() {
 	requestBody, error := json.Marshal(map[string]string{
 		"name":    "Burak",
 		"country": "Turkey",
 	})
+
+	if error != nil {
+		log.Fatalln(error)
+	}
+
+	response, err := http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(requestBody))
+
+	defer response.Body.Close()
 }
