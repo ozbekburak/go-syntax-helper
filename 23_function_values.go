4e42c450283658b86601ea7bc15e60c037c2a672
Burak Kaan <ozbekburakkaan@outlook.com>
2020-12-17 23:35:40 +0300

    update comments

--- 23_function_values.go
+++ 23_function_values.go
@@ -8,8 +8,8 @@ import (
 // a tour of go örneğinin aynısı kullanılmıştır.
 // argüman olarak fonksiyon alıyoruz, float64 tipinde veri geri döndürüyoruz.
 // argüman olarak aldığımız fonksiyonun dönüş değerini de belirttiğimize dikkat edelim.
-func compute(fn func(float64, float64) float64) float64 {
-	return fn(3, 4)
+func compute(fn func(float64, float64) float64, x, y int) float64 {
+	return fn(x, y)
 }
 
 func main() {
@@ -18,5 +18,5 @@ func main() {
 	}
 	fmt.Println("Hypotenuse of 5 and 12:", hypotenuse(5, 12))
 
-	fmt.Println("Hypotenuse of 3 and 4 (function -> function): ", compute(hypotenuse))
+	fmt.Println("Hypotenuse of 3 and 4 (function -> function): ", compute(hypotenuse(3, 4)))
 }
